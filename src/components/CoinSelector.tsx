53e482d74fd2da4cf5ff42d6c8d9adfbe5e6f7d3
andy78644 <andy690100391@gmail.com>
2025-01-09 15:15:52 +0800

    Remove PSM coin

--- src/components/CoinSelector.tsx
+++ src/components/CoinSelector.tsx
@@ -1,5 +1,5 @@
 import { Button, Flex } from "@radix-ui/themes";
-import { COINS_TYPE_LIST } from "../constants";
+import { COINS_TYPE_LIST, PSM_POOL_IDS} from "../constants";
 import { Dispatch, SetStateAction } from 'react';
 
 interface CoinSelectorProps {
@@ -11,20 +11,22 @@ interface CoinSelectorProps {
 export function CoinSelector({ selectedCoin, onSelectCoin }: CoinSelectorProps) {
   return (
     <Flex 
-        gap="2" 
-        wrap="wrap" 
-        style={{ 
-            maxWidth: "100%",
-            justifyContent: "center"
-        }}
+      gap="2" 
+      wrap="wrap" 
+      style={{ 
+        maxWidth: "100%",
+        justifyContent: "center"
+      }}
     >
-      {Object.entries(COINS_TYPE_LIST).map(([key, _]) => (
+      {Object.entries(COINS_TYPE_LIST)
+      .filter(([key]) => !Object.keys(PSM_POOL_IDS).includes(key))
+      .map(([key, _]) => (
         <Button 
-          key={key}
-          onClick={() => onSelectCoin(key as keyof typeof COINS_TYPE_LIST)}
-          variant={selectedCoin === key ? "solid" : "outline"}
+        key={key}
+        onClick={() => onSelectCoin(key as keyof typeof COINS_TYPE_LIST)}
+        variant={selectedCoin === key ? "solid" : "outline"}
         >
-          {key}
+        {key}
         </Button>
       ))}
     </Flex>
